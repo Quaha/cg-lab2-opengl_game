d7211d9c19956d75531e839d70223f15681d4f26
Quaha <ArtemRomanov2017@yandex.ru>
2025-04-06 22:12:21 +0300

    GameObject Ver No1

--- GameObject.cs
+++ GameObject.cs
@@ -1,1 +1,24 @@
-﻿
+﻿namespace Game {
+    public class GameObject {
+
+        private ArrayObject VAO;
+        private BufferObject VBO;
+        private BufferObject texture_VBO;
+        private BufferObject EBO;
+
+        private int vertex_count;
+        private int texture_ID;
+
+        public GameObject() {
+
+        }
+
+        public void render() {
+
+        }
+
+        public void delete() {
+
+        }
+    }
+}
